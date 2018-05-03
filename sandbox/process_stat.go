20ec9a144abc56d71a47ea611c7736ac7dddb1f8
shiyicode <458241107@qq.com>
2018-05-04 04:02:35 +0800

    a

--- sandbox/process_stat.go
+++ sandbox/process_stat.go
@@ -28,20 +28,34 @@ func init() {
 	PAGESIZE = int64(syscall.Getpagesize())
 }
 
-func GetResourceUsage(pid int) (ok bool, vm int64, rss int64,
-	rt int64, ct int64) {
-	stat, err := os.Open("/proc/" + strconv.Itoa(pid) + "/stat")
+func readProcFile(filename string) ([]byte, error) {
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		if os.IsNotExist(err) {
-			return
-		} else {
-			panic(err)
+			return nil, nil
 		}
+
+		if perr, ok := err.(*os.PathError); ok && perr.Err == syscall.ESRCH {
+			return nil, nil
+		}
+
+		return nil, err
 	}
-	bs, err := ioutil.ReadAll(stat)
+
+	return data, nil
+}
+
+func GetResourceUsage(pid int) (ok bool, vm int64, rss int64,
+	rt int64, ct int64) {
+	bs, err := readProcFile("/proc/" + strconv.Itoa(pid) + "/stat")
 	if err != nil {
 		panic(err)
 	}
+	if bs == nil {
+		ok = true
+		return
+	}
+
 	//virtual memory size is 23nd paramater in the stat file,in bytes
 	vm, err = strconv.ParseInt(strings.Split(string(bs), " ")[22], 10, 64)
 	if err != nil {
